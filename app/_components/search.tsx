27fc1c2b7eb9437e8c96f6924749443e0596b455
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-04-30 19:29:12 -0300

    feat: add restaurant list and restaurant item components

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -5,7 +5,10 @@ import { Input } from "./ui/input";
 const Search = () => {
   return (
     <div className="flex gap-2">
-      <Input placeholder="Buscar restaurantes" className="border-none" />
+      <Input
+        placeholder="Buscar restaurantes"
+        className="border-none outline-none hover:ring-2 hover:ring-secondary focus-visible:ring-2 focus-visible:ring-primary"
+      />
       <Button size="icon">
         <SearchIcon size={18} />
       </Button>
