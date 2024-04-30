040ca568db95c32553c3de40bad52f0faa0ddbc3
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-04-30 12:37:43 -0300

    feat: add promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Content from "./_components/content";
 import Header from "./_components/header";
@@ -12,6 +13,17 @@ const Home = () => {
       </Content>
       <Content>
         <CategoryList />
+      </Content>
+
+      <Content>
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
       </Content>
     </>
   );
